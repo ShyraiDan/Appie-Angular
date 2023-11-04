520c7ceb0d1b1bf693b2fe1f49b9d80a0de7825a
ShirayDan <dan.shirayy@gmail.com>
2023-11-04 13:00:00 +0200

    add template and styles for navigation component

--- src/app/header/navigation/navigation.component.ts
+++ src/app/header/navigation/navigation.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.css'],
 })
 export class NavigationComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  navbar = [
+    { title: 'Home' },
+    { title: 'Service' },
+    { title: 'Pages' },
+    { title: 'News' },
+    { title: 'Contact' },
+  ];
 
+  nums = [1, 2, 3, 4, 5];
 }
