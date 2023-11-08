51f7d73428024653b6e00899ff24d3dd362d2805
ShirayDan <dan.shirayy@gmail.com>
2023-11-08 12:43:17 +0200

    created ui and styles for download-app component

--- src/app/main/download-app/download-app.component.ts
+++ src/app/main/download-app/download-app.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-download-app',
   templateUrl: './download-app.component.html',
-  styleUrls: ['./download-app.component.css']
+  styleUrls: ['./download-app.component.css'],
 })
 export class DownloadAppComponent implements OnInit {
+  data = [
+    {
+      title: 'Android',
+      device: 'Android',
+      photo: '../../../assets/i/download-thumb-1.png',
+    },
+    {
+      title: 'iOS & iPadOS',
+      device: 'iOS',
+      photo: '../../../assets/i/download-thumb-2.png',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
