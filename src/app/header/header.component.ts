da2fbb0338113732c2a24498c323281f2f672e1f
ShirayDan <dan.shirayy@gmail.com>
2023-11-19 12:22:44 +0200

    fixed burger menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  Renderer2,
+} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  burgerOpen = false;
 
-  constructor() { }
+  constructor(private renderer: Renderer2) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  burgerClick(event: HTMLDivElement) {
+    document.body.classList.toggle('lock');
+    this.burgerOpen = !this.burgerOpen;
   }
-
 }
