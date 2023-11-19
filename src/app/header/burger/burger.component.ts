5ae2a9dd6dd6a43588bf1b79ffe55a66512e6c05
ShirayDan <dan.shirayy@gmail.com>
2023-11-19 12:38:45 +0200

    fixed accordion in burger menu

--- src/app/header/burger/burger.component.ts
+++ src/app/header/burger/burger.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class BurgerComponent implements OnInit {
   constructor() {}
 
-  selected = 0;
+  selected = -1;
   acordeon = [
     { title: 'Home' },
     { title: 'Service' },
@@ -16,13 +16,33 @@ export class BurgerComponent implements OnInit {
     { title: 'News' },
     { title: 'Contact' },
   ];
+
   social = [
     { spanCl: 'icon icon-facebook-blue' },
     { spanCl: 'icon icon-twitter-blue' },
     { spanCl: 'icon icon-instagram-blue' },
     { spanCl: 'icon icon-linkedin2-blue' },
   ];
+
   nums = [1, 2, 3, 4, 5];
 
+  footerRight = [
+    { icon: 'mail', text: 'support@appie.com' },
+    { icon: 'phone', text: '+(642) 342 762 44' },
+    {
+      icon: 'location_on',
+      text: '442 Belle Terre St Floor 7, San Francisco, AV 4206',
+    },
+  ];
+
   ngOnInit(): void {}
+
+  toggleAccordion(num: number) {
+    if (this.selected === num) {
+      this.selected = -1;
+      return;
+    }
+
+    this.selected = num;
+  }
 }
